bf1b2ea1c11394a1ea2925f55280fff664d02d3c
Ryan Hang <ryan.hang3r@gmail.com>
2024-08-03 22:42:00 -0700

    First Commit of Whole Application

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Customizer from './pages/Customizer';
+import Canvas from './canvas';
+import Home from'./pages/Home';
+
+function App() {
+
+  return (
+    <main className="app transition-all ease-in"> 
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
